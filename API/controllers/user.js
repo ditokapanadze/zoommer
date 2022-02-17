5a2a4721fe65d03af7b776ba160978c86b2c0aed
ditokapanadze <66876572+ditokapanadze@users.noreply.github.com>
2022-02-17 10:22:01 +0400

    verifytoken middleware added

--- API/controllers/user.js
+++ API/controllers/user.js
@@ -3,6 +3,7 @@ import User from "../models/user.js";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
+import verifyToken from "../Middleware/verifyToken.js";
 dotenv.config();
 
 export const registerUser = async (req, res) => {
@@ -16,12 +17,13 @@ export const registerUser = async (req, res) => {
       });
     }
     const hash = await bcrypt.hash(password, 12);
+
     const newUser = await User.create({
       email,
       password: hash,
-      name: name,
+      name,
     });
-    console.log(process.env.JWT_SECRET);
+
     const token = jwt.sign(
       { email: newUser.email, id: newUser._id },
       "CnsyiR1YYwsckdRT0kiDntMbNenoeg2eTTzDpRoJuFnjMHR7H6/wOFhuqQ1+X+mtB8mSsDiqeIRXINIKeqinqLd3CZGVYsaJosCPVD+c9EPS9X9utCeg3etVPlJ81PKmrxKVO7rKp95H4Gdh1pE8IMTanLQHPmYwxNm3+JsAw7hRkyxaGc/+xQ4QSAgK/nSzchmCnOJVJ9tSECi2waNMTVkhH8Kc4AA6MhC3XPoI0gFSTL8SMIU0B3JHJVYAyFRKzZgEANHqhEWnkDXRhkFqBzTDvrBGZKqSmu5BNs1/l6R4ge4YBDG44ZJXbE7Uf7lpe74yxq8DIJzSxeqKStNwlw==",
@@ -62,4 +64,8 @@ export const signIn = async (req, res) => {
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
+};
+
+export const ttt = async (req, res, next) => {
+  console.log(req.user);
 };
--- API/routes/user.js
+++ API/routes/user.js
@@ -1,7 +1,10 @@
 import express from "express";
-import { registerUser, signIn } from "../controllers/user.js";
+import { registerUser, signIn, ttt } from "../controllers/user.js";
+import verifyToken from "../Middleware/verifyToken.js";
+
 const router = express.Router();
 
 router.post("/register", registerUser);
 router.post("/signin", signIn);
+router.get("/test", verifyToken, ttt);
 export default router;
