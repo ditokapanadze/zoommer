9a60c0b51f7f86b9857224313193f16605af2347
ditokapanadze <66876572+ditokapanadze@users.noreply.github.com>
2022-02-18 13:55:38 +0400

    basic filters added, needs to be modified

--- API/routes/product.js
+++ API/routes/product.js
@@ -3,6 +3,7 @@ import {
   addProduct,
   getAllProducts,
   searchProduct,
+  filterProduct,
 } from "../controllers/product.js";
 
 const router = express.Router();
@@ -10,4 +11,5 @@ const router = express.Router();
 router.post("/addproduct", addProduct);
 router.get("/getall", getAllProducts);
 router.get("/search", searchProduct);
+router.get("/filter", filterProduct);
 export default router;
